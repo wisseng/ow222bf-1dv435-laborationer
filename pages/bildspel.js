19a9b813377a25548018f1a1ebaea9a10924b289
OpenShift guest <ow222bf@student.lnu.se>
2013-12-20 11:37:16 -0500

    gjort klart bildspel

---
+++ pages/bildspel.js
@@ -0,0 +1,24 @@
+
+
+var image1=new Image ()
+image1.src="../pics/grf.png"
+
+var image2=new Image ()
+image2.src="../pics/giraff2.png"
+
+var image3=new Image ()
+image3.src="../pics/gir_side.png"
+
+var step=1
+function slideit (){
+    document.images.slide.src=eval ("image"+step+".src");
+    if(step<3)
+    step++
+    else
+    step=1
+    setTimeout("slideit()",2000)
+}
+slideit();
+
+
+
--- pages/javascript.js
+++ pages/javascript.js
@@ -1,16 +1,11 @@
+
+/* hide and show */
+
+
 function toggle_visibility(id) {
-    var e = document.getElementById(id);
-    if (e.style.display == 'block') e.style.display = 'none';
-    else e.style.display = 'block';
+    var tabell = document.getElementById(id);
+    if (tabell.style.display == 'block') tabell.style.display = 'none';
+    else tabell.style.display = 'block';
     
-    hideAllBut(id);
 }
 
-function hideAllBut(id) {
-    var lists = document.querySelectorAll('.list');
-    for (var i = lists.length; i--; ) {
-        if (lists[i].id != id) {
-            lists[i].style.display = 'none';
-        }
-    }
-}
